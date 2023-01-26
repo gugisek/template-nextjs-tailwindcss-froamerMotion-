6ab98460b3fe30ddcdebedc63c75ed27c77672a1
gugisek <gugisek@gmail.com>
2023-01-26 12:13:53 +0100

    Add framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import TestLanding from '@components/test-landing'
+import { Lexend } from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Lexend({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <section style={inter.style} className='bg-black text-white'>
-      Hello World
+      <TestLanding />
     </section>
   )
 }
